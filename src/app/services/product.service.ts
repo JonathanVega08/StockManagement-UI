3b2aee8e7e4df1c1fb4bcacfd9191c9291e2b991
Jonathan Vega <jonathan.vega@outlook.com>
2020-06-03 11:12:29 -0500

    Renamed product model.

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { ProductModel } from '../models/product.model';
+import { ProductModel } from '../models/product-model';
 import { environment } from '../../environments/environment';
 
 
