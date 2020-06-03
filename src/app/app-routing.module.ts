1f0d69b96db622c4b06a1550cc2badb374c6b929
Jonathan Vega <jonathan.vega@outlook.com>
2020-06-03 11:11:02 -0500

    Added new components to routing and home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AddStockComponent } from './add-stock/add-stock.component';
+import { EditStockComponent } from './edit-stock/edit-stock.component';
 
 
 const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent}
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'add', component: AddStockComponent },
+  { path: 'edit/:productId', component: EditStockComponent }
 ];
 
 @NgModule({
